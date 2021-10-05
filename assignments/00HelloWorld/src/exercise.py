2ba10037d3eea070912fb75eeeb4ea924ec85008
rquinteroTecSin <ricardo.quintero@tec.mx>
2021-10-05 14:36:49 +0000

    Listo !

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,23 @@
+def imprime_nombreSamanta():
+    print('Samantha')
+    pass
+
+def imprime_nombreJuan():
+    print("Juan")
+
+def imprime_nombreRuben():
+    print("Ruben")
+    
+
+def imprime_nombreSamuel():
+    print("Samuel")
+    
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    imprime_nombreSamanta()
+    imprime_nombreJuan()
+    imprime_nombreRuben()
+    imprime_nombreSamuel()
 
 if __name__=='__main__':
     main()
